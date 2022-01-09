15005aeb8642b236fde207c121411059aec27edc
Stacey Wang <zw219@duke.edu>
2022-01-09 14:44:22 -0500

    Chapter 14

--- Test.java
+++ Test.java
@@ -70,6 +70,26 @@ public class Test {
         assertEquals(Money.dollar(1), result);
     }
 
+    @org.junit.Test
+    public void testReduceMoneyDifferentCurrency() {
+        Bank bank= new Bank();
+        bank.addRate("CHF", "USD", 2);
+        Money result= bank.reduce(Money.franc(2), "USD");
+        assertEquals(Money.dollar(1), result);
+    }
+
+//    @org.junit.Test
+//    public void testArrayEquals() {
+//        assertEquals(new Object[] {"abc"}, new Object[] {"abc"});
+//    }
+
+    @org.junit.Test
+    public void testIdentityRate() {
+        assertEquals(1, new Bank().rate("USD", "USD"));
+    }
+
+
+
 
 
 
