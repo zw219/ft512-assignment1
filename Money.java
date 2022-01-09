062a6e97c583c2d455a6895fd98a363540e8be77
Stacey Wang <zw219@duke.edu>
2022-01-08 23:10:38 -0500

    Chapter 8

--- Money.java
+++ Money.java
@@ -1,5 +1,14 @@
-public class Money {
+abstract class Money {
     protected int amount;
+
+    abstract Money times(int mutiplier);
+
+    static Money dollar(int amount) {
+        return new Dollar(amount);
+    }
+    static Money franc(int amount) {
+        return new Franc(amount);
+    }
 
     public boolean equals(Object object) {
         Money money = (Money) object;
