efe087d0e99189333bb2fa67ddd31e0dc974b188
Stacey Wang <zw219@duke.edu>
2022-01-09 12:43:26 -0500

    Chapter 12

--- Money.java
+++ Money.java
@@ -1,4 +1,4 @@
-class Money {
+class Money implements Expression{
     protected int amount;
     protected  String currency;
 
@@ -31,6 +31,13 @@ class Money {
     public String toString() {
         return amount + " " + currency;
     }
+
+
+    Expression plus(Money addend) {
+        return new Money(amount + addend.amount, currency);
+    }
+
+
 
 
 }
--- Test.java
+++ Test.java
@@ -35,4 +35,15 @@ public class Test {
     public void testDifferentClassEquality() {
         assertTrue(new Money(10, "CHF").equals(new Franc(10, "CHF")));
     }
+
+    @org.junit.Test
+    public void testSimpleAddition() {
+        Money five= Money.dollar(5);
+        Expression sum= five.plus(five);
+        Bank bank= new Bank();
+        Money reduced= bank.reduce(sum, "USD");
+        assertEquals(Money.dollar(10), reduced);
+    }
+
+
 }
