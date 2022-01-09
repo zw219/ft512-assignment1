cf4ea0ec737a34f5d62db20dd568273967d0207d
Stacey Wang <zw219@duke.edu>
2022-01-08 22:49:26 -0500

    Chapter 7

--- Test.java
+++ Test.java
@@ -14,9 +14,10 @@ public class Test {
     @org.junit.Test
     public void testEquality() {
         assertTrue(new Dollar(5).equals(new Dollar(5)));
-        assertTrue(new Dollar(5).equals(new Dollar(6)));
+        assertFalse(new Dollar(5).equals(new Dollar(6)));
         assertTrue(new Franc(5).equals(new Franc(5)));
-        assertTrue(new Franc(5).equals(new Franc(6)));
+        assertFalse(new Franc(5).equals(new Franc(6)));
+        assertFalse(new Franc(5).equals(new Dollar(5)));
     }
 
     @org.junit.Test
